d9e397f0a2cf043e94bed448e19c1377f19b7ed7
Antoine Bonnard <Antoine.bonnard@etu.imt-nord-europe.fr>
2024-01-28 20:09:59 +0100

    Add Protecteur.java

---
+++ src/main/java/org/HearthStone/personnages/monstres_classes/Protecteur.java
@@ -0,0 +1,9 @@
+package org.HearthStone.personnages.monstres_classes;
+
+import org.HearthStone.personnages.Monstre;
+
+public class Protecteur extends Monstre {
+    public Protecteur(int id, String nom, int pv, int forceAdaptative) {
+        super(id, nom, pv, forceAdaptative);
+    }
+}
